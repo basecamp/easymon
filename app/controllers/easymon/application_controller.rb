64c5d6ac347c001c066e73e5e815f4aefb6be3b0
Nathan Anderson <andnat@gmail.com>
2013-11-25 08:28:09 -0500

    Moved exception to ChecksController

--- app/controllers/easymon/application_controller.rb
+++ app/controllers/easymon/application_controller.rb
@@ -1,10 +1,4 @@
 module Easymon
   class ApplicationController < ActionController::Base
-    rescue_from Easymon::Repository::NoSuchCheck do |e|
-      respond_to do |format|
-         format.any(:text, :html) { render text: e.message, status: :not_found }
-         format.json { render json: e.message, status: :not_found }
-      end
-    end
   end
 end
--- app/controllers/easymon/checks_controller.rb
+++ app/controllers/easymon/checks_controller.rb
@@ -2,13 +2,20 @@ require_dependency "easymon/application_controller"
 
 module Easymon
   class ChecksController < ApplicationController
+    rescue_from Easymon::Repository::NoSuchCheck do |e|
+      respond_to do |format|
+         format.any(:text, :html) { render text: e.message, status: :not_found }
+         format.json { render json: e.message, status: :not_found }
+      end
+    end
+    
     def index
       checks = Easymon::Repository.all
       checks.run
 
       respond_to do |format|
-         format.any(:text, :html) { render text: checks, status: set_status(checks) }
-         format.json { render json: checks, status: set_status(checks) }
+         format.any(:text, :html) { render text: checks, status: checks.response_status }
+         format.json { render json: checks, status: checks.response_status }
       end
     end
 
@@ -17,15 +24,9 @@ module Easymon
       check.run
       
       respond_to do |format|
-         format.any(:text, :html) { render text: check, status: check.success? ? :ok : 503 }
-         format.json { render json: check, status: check.success? ? :ok : 503 }
+         format.any(:text, :html) { render text: check, status: check.response_status }
+         format.json { render json: check, status: check.response_status }
       end
     end
-    private
-      def set_status(checks)
-        return :ok if checks.success?
-        return :ok if checks.has_critical? && checks.critical_success?
-        503
-      end
   end
 end
