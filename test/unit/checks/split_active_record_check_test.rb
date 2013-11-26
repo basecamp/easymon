96559ebbcb9970cd4f56af93c00a0a685631b704
Nathan Anderson <andnat@gmail.com>
2013-11-25 19:26:34 -0500

    Change SplitActiveRecordCheck to take a block on initialization

--- test/unit/checks/split_active_record_check_test.rb
+++ test/unit/checks/split_active_record_check_test.rb
@@ -2,18 +2,13 @@ require 'test_helper'
 
 class SplitActiveRecordCheckTest < ActiveSupport::TestCase
   
-  def setup
-    # Connection judo to establish an independent connection to a separate db.
-    # This is normally done in your app code or something like it. 
-    slave_spec = ActiveRecord::Base.configurations["#{Rails.env}_slave"]
-    config = ActiveRecord::Base::ConnectionSpecification.new(slave_spec, "#{slave_spec['adapter']}_connection")
-    handler = ActiveRecord::ConnectionAdapters::ConnectionHandler.new
-    handler.establish_connection(self.class.name, config)
-    @slave_connection = handler.retrieve_connection(SplitActiveRecordCheckTest)
-  end
-  
   test "#run sets success conditions on successful run" do
-    check = create_check
+    master = ActiveRecord::Base.connection
+    Easymon::Base.establish_connection
+    slave = Easymon::Base.connection
+    
+    check = Easymon::SplitActiveRecordCheck.new { [master, slave] }
+    check.name = "SplitActiveRecord"
     check.run
     
     assert_equal("Master: Up - Slave: Up", check.message)
@@ -22,29 +17,52 @@ class SplitActiveRecordCheckTest < ActiveSupport::TestCase
   end
   
   test "#run sets failed conditions when slave is down" do
-    @slave_connection.stubs(:select_value).raises("boom")
-    check = create_check
-    check.run
+    master = ActiveRecord::Base.connection
+    Easymon::Base.establish_connection
+    slave = Easymon::Base.connection
+
+    slave.stubs(:select_value).raises("boom")
     
+    check = Easymon::SplitActiveRecordCheck.new { [master, slave] }
+    check.name = "SplitActiveRecord"
+    check.run
+
     assert_equal("Master: Up - Slave: Down", check.message)
     assert_equal(true, check.failed)
     assert_equal(false, check.success?)
   end
   
   test "#run sets failed conditions when master is down" do
-    ActiveRecord::Base.connection.stubs(:select_value).raises("boom")
-    check = create_check
+    master = ActiveRecord::Base.connection
+    Easymon::Base.establish_connection
+    slave = Easymon::Base.connection
+
+    master.stubs(:select_value).raises("boom")
+    
+    check = Easymon::SplitActiveRecordCheck.new { [master, slave] }
+    check.name = "SplitActiveRecord"
     check.run
     
     assert_equal("Master: Down - Slave: Up", check.message)
     assert_equal(true, check.failed)
     assert_equal(false, check.success?)
   end
-  
-  private
-  def create_check
-    check = Easymon::SplitActiveRecordCheck.new(ActiveRecord::Base.connection, @slave_connection)
-    check.name = "ActiveRecord"
-    return check
+end
+
+# This is modeled after SignalId::Base and would be done in the host app
+module Easymon
+  class Base < ActiveRecord::Base
+    def establish_connection(spec = nil)
+      if spec
+        super
+      elsif config = Easymon.database_configuration
+        super config
+      end
+    end
+    
+    def database_configuration
+      env = "#{Rails.env}_slave"
+      config = YAML.load_file(Rails.root.join('config/database.yml'))[env]
+    end
   end
 end
