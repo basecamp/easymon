802fbfb26ca757f8f2f2dc2c21a85fba4e144efc
Nathan Anderson <andnat@gmail.com>
2013-11-27 12:32:52 -0500

    Add a Result object

--- lib/easymon.rb
+++ lib/easymon.rb
@@ -2,6 +2,8 @@ require "easymon/engine"
 require "easymon/check"
 require "easymon/checklist"
 require "easymon/repository"
+require "easymon/result"
+
 require "easymon/checks/active_record_check"
 require "easymon/checks/split_active_record_check"
 require "easymon/checks/redis_check"
