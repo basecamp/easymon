32cf0749abda7c3c55aa46f8051f6baf68ff7812
Jeffrey Hardy <packagethief@gmail.com>
2015-02-24 21:27:58 -0500

    Shed the rest-client dependency in favor of using net/http

--- lib/easymon/checks/http_check.rb
+++ lib/easymon/checks/http_check.rb
@@ -1,4 +1,4 @@
-require "restclient"
+require 'net/https'
 
 module Easymon
   class HttpCheck
@@ -19,15 +19,22 @@ module Easymon
     end
     
     private
-      def http_up?(config_url)
-        response = RestClient::Request.execute(
-                                      :method => :head, 
-                                      :url => config_url,
-                                      :timeout => 5,
-                                      :open_timeout => 5)
-        true
+      def http_up?(url)
+        http_head(url).is_a?(Net::HTTPSuccess)
       rescue Exception
         false
+      end
+
+      def http_head(url)
+        uri = URI.parse(url)
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = uri.is_a?(URI::HTTPS)
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http.open_timeout = 5
+        http.read_timeout = 5
+
+        http.request Net::HTTP::Head.new(uri.request_uri)
       end
   end
 end
