4e759345d91c76adc22a4febbf8db180214e7836
Nathan Anderson <andnat@gmail.com>
2016-01-11 16:56:52 -0500

    Provide abstract before_method to avoid before_filter deprecation notices

--- app/controllers/easymon/checks_controller.rb
+++ app/controllers/easymon/checks_controller.rb
@@ -3,7 +3,17 @@ require "benchmark"
 
 module Easymon
   class ChecksController < ApplicationController
-    before_filter :authorize_request
+    # Rails 5+ deprecates `before_filter` in favor of `before_action`. Alias
+    # the latter for forward compatibility.
+    unless defined?(before_action)
+      class << self
+        %w( before ).each do |callback|
+          alias_method :"#{callback}_action", :"#{callback}_filter"
+        end
+      end
+    end
+
+    before_action :authorize_request
 
     rescue_from Easymon::NoSuchCheck do |e|
       respond_to do |format|
